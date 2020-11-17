92e6dfe0fbcc7639f96fbdd2af952cedd30ca4ae
Game Over <janessarefong@gmail.com>
2020-11-16 22:29:02 -0800

    fix card styling for portfolio page

--- my-portfolio-react/src/pages/Portfolio.js
+++ my-portfolio-react/src/pages/Portfolio.js
@@ -3,17 +3,43 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
+const styles = {
+    iconSize: {
+        height: 30
+    },
+    card: {
+        backgroundColor: "#ffcccc",
+        borderRadius: 20,
+        maxWidth: "100%",
+        opacity: 0.9,
+        height: "auto"
+    },
+    cardHeader: {
+        fontFamily: "DM Serif Text, serif"
+    },
+    inputField: {
+        backgroundColor: "#ffe8e8",
+        borderRadius: 12
+    },
+    contactInfo: {
+        color: "black"
+    },
+    paddingStyle: {
+        paddingBottom: 100
+    }
+};
+
 
 function Portfolio() {
     return(
         <React.Fragment>
             <Navbar />
             <main className="container">
-                <section className="row mx-auto px-auto" style="padding-bottom: 100px;">
+                <section className="row mx-auto px-auto" style={styles.paddingStyle}>
                     <div className="col-xs-12 col-sm-12 col-md-8 col-lg-8 p-0 rounded">
-                        <div className="card m-5 cardPage" style="max-width: 100%; opacity:.9; height:auto">
+                        <div className="card m-5" style={styles.card}>
                             <div className="card-body">
-                                <h2 className="card-title text-info font-weight-bold" style="font-family: 'DM Serif Text', serif;">Portfolio</h2>
+                                <h2 className="card-title text-info font-weight-bold" style={styles.cardHeader}>Portfolio</h2>
                                 <hr/>
                                 <div className="row mx-auto px-auto">
                                 {/* add projects */}
@@ -21,7 +47,7 @@ function Portfolio() {
                                 </div>
                             </div>
                         </div>
-                        <div className="col-md-4 col-lg-3 "></div>
+                        <div className="col-md-4 col-lg-3"></div>
                     </div>
                 </section>
             </main>
