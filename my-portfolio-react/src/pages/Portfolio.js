04ed9f7da337e3600f8badef296440d37c1fee1f
Game Over <janessarefong@gmail.com>
2020-11-16 22:27:10 -0800

    add project component and add content on portfolio page

--- my-portfolio-react/src/pages/Portfolio.js
+++ my-portfolio-react/src/pages/Portfolio.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Container from "../components/Container"
+// import Container from "../components/Container"
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
@@ -8,10 +8,23 @@ function Portfolio() {
     return(
         <React.Fragment>
             <Navbar />
-                <Container>
-            
-  
-                </Container>
+            <main className="container">
+                <section className="row mx-auto px-auto" style="padding-bottom: 100px;">
+                    <div className="col-xs-12 col-sm-12 col-md-8 col-lg-8 p-0 rounded">
+                        <div className="card m-5 cardPage" style="max-width: 100%; opacity:.9; height:auto">
+                            <div className="card-body">
+                                <h2 className="card-title text-info font-weight-bold" style="font-family: 'DM Serif Text', serif;">Portfolio</h2>
+                                <hr/>
+                                <div className="row mx-auto px-auto">
+                                {/* add projects */}
+                
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-4 col-lg-3 "></div>
+                    </div>
+                </section>
+            </main>
           <Footer />
         </React.Fragment>
          
