bf97dbed63dd53e205f79e294d3b045a76b59713
Game Over <janessarefong@gmail.com>
2020-11-17 22:23:34 -0800

    fix styling of navlinks

--- my-portfolio-react/src/components/Navbar/index.js
+++ my-portfolio-react/src/components/Navbar/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./style.css";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+
+
 
 function Navbar() {
     return(
     <header>
-        <nav className="navbar navbar-expand-lg navbar-light">
+        <nav className="navbar navbar-expand-lg navbar-dark">
             <h1 className="p-2 mb-2 ml-3" id="jfName">
-                <a className="navbar-brand pl-3 px-1" id="jfLink" href="/">JANESSA FONG</a>
+                <NavLink to="/" className="navbar-brand pl-3 px-1" id="jfLink" >JANESSA FONG</NavLink>
             </h1>
 
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -17,15 +19,15 @@ function Navbar() {
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav ml-md-auto">
                     <li className="nav-item">
-                        <Link to="/contact" className="nav-link">Contact</Link>
+                        <NavLink to="/contact" className="nav-link links">Contact</NavLink>
                     </li>
 
                     <li className="nav-item">
-                        <Link to="/portfolio" className="nav-link">Portfolio</Link>
+                        <NavLink to="/portfolio" className="nav-link links">Portfolio</NavLink>
                     </li>
 
                     <li className="nav-item">
-                        <Link to="/" className="nav-link active">About<span className="sr-only">(current)</span></Link>
+                        <NavLink to="/about" className="nav-link links">About</NavLink>
                     </li>
 
                 </ul>
