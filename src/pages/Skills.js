efac6a6065e764f81b95b267bce4b3716583b8b1
Game Over <janessarefong@gmail.com>
2021-04-19 14:44:55 -0700

    add skills and logos

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -18,14 +18,27 @@ function Skills() {
                     <div className="col-sm-12 col-md-12 col-lg-10 p-5 rounded">
                         <div className="card" data-aos="flip-down" data-aos-duration="3000">
                             <div className="card-body">
-                                <h2 className="card-title cardHeader">About Me</h2>
+                                <h2 className="card-title cardHeader">Technical Skills</h2>
                                 <hr/>
-                              
-                               
+                                    <a href="https://git-scm.com/" target="_blank" rel="noreferrer">
+                                        <img src="../../assets/Git-Logo-Black.png" className="mr-2" width="100px" alt="git logo" />
+                                    </a>
+                                    
+                                    <a href="https://www.mysql.com" target="_blank" rel="noreferrer">
+                                        <img src="../../assets/logo-mysql.png" className="mr-2" width="100px" alt="mysql logo" />
+                                    </a>
+
+                                    <a href="https://www.mongodb.com/" target="_blank" rel="noreferrer">
+                                        <img src="../../assets/MongoDB_LogoStacked_FullColorBlack_RGB.png" className="mr-2" width="100px" alt="mongoDB logo" />
+                                    </a>
+
+                                    <a href="https://www.heroku.com/" target="_blank" rel="noreferrer">
+                                        <img src="../../assets/heroku-logotype-vertical-purple.png" className="mr-2" width="100px" alt="heroku logo" />
+                                    </a>
                             </div>
                         </div>
                     </div>
-                    <div className="col-lg-2"></div>
+                    {/* <div className="col-lg-2"></div> */}
                 </section>
             </main>
             <Footer />
