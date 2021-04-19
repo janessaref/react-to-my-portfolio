730f0baf24a1b1eaa564e343899359b498966516
Game Over <janessarefong@gmail.com>
2021-04-19 15:13:07 -0700

    modified and updated resume pdf

--- src/pages/About.js
+++ src/pages/About.js
@@ -52,7 +52,7 @@ function About() {
                                         </a>
                                         <br/>
                                         <br/>
-                                        <a href="../../assets/Resume_(Fong).pdf" target="_blank" rel="noreferrer" id="resume">
+                                        <a href="../../assets/Janessa_Fong_Resume.pdf" target="_blank" rel="noreferrer" id="resume">
                                             <svg width="1.5em" height="1.5em" viewBox="0 0 16 16" className="bi bi-file-earmark-person mb-1 mr-1" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                                 <path d="M4 0h5.5v1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5h1V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2z"/>
                                                 <path d="M9.5 3V0L14 4.5h-3A1.5 1.5 0 0 1 9.5 3z"/>
